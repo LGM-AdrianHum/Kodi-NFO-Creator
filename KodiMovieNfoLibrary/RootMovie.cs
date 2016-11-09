f85a7d1626dd4e5284190285c2610f0fb355bc9c
Adrian Hum <a@littlegreenmartian.net>
2016-11-10 07:38:35 +1100

    Updates to Serializer

--- KodiMovieNfoLibrary/RootMovie.cs
+++ KodiMovieNfoLibrary/RootMovie.cs
@@ -196,31 +196,4 @@ namespace KodiMovieNfoLibrary
             return rm;
         }
     }
-
-    [XmlRoot("actor")]
-    public class ActorName
-    {
-        public ActorName()
-        {
-        }
-
-        public ActorName(string n, string r)
-        {
-            Name = n;
-            Role = r;
-        }
-
-        [XmlElement("name")]
-        public string Name { get; set; }
-
-        [XmlElement("role")]
-        public string Role { get; set; }
-    }
-
-    [XmlRoot("fileinfo")]
-    public class VideoStreamDetails
-    {
-        [XmlElement("streamdetails")]
-        public string StreamDetails { get; set; }
-    }
 }
